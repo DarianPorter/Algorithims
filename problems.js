e3ed068618c52eac02727cd46c7c82b81784b101
darianporter <darianporter@icloud.com>
2019-09-25 15:19:11 -0400

    finished problem

--- problems.js
+++ problems.js
@@ -1,13 +1,26 @@
-// method defined on String Prototype, off sets string by x ammount then return jumbled string 
-// input "abca".jumbleSort(3)
-// output "defd"
 String.prototype.jumbleSort = function(x){
     let alpha = "abcdefghijklmnopqrstuvwxyz"
     let offSet = x % 26;
-    let newS = ""
+    let newS = "";
     for(let i = 0; i < this.length; i++){
-        let index = alpha.indexOf(this[i]) + offSet
-        newS += alpha[index]
+        let index = alpha.indexOf(this[i]) + offSet;
+        newS += alpha[index];
     }
-    return newS
+    return newS;
 }
+
+Array.prototype.bubbleSort = function(){
+    let sorted = false
+    while(!sorted){
+        sorted = true 
+        for(let i = 0; i < this.length; i++){
+            if(this[i] > this[i+1]){
+                [this[i], this[i+1]] = [this[i + 1], this[i]]
+                sorted = false
+            }
+        }
+    }
+    return this 
+}   
+let a = [1, 3, 2, 4, 2, 1]
+console.log(a.bubbleSort())
